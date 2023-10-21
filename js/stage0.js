46d55e9c6e4270c91f7e05f293d93c410c340374
Akshaj Aggarwal <f20220540@hyderabad.bits-pilani.ac.in>
2023-10-21 19:15:19 +0530

    Thankyou and stage 0

--- js/stage0.js
+++ js/stage0.js
@@ -1,3 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var thankform = document.querySelector('.thankyou');
+    var stageform = document.querySelector('.stage0-modal');
+    var stagelink = document.querySelector('.stage a');
+
+    thankform.style.display = 'block';
+    stageform.style.display = 'none';
+
+    stagelink.addEventListener('click', function(event) {
+             event.preventDefault();
+             thankform.style.display = 'none';
+             stageform.style.display = 'block';
+             //forgotpassword.style.display = 'none';
+         });
+});
 console.log("running stage0.js")
 import{ getAuth, onAuthStateChanged, signOut} from "https://www.gstatic.com/firebasejs/10.5.0/firebase-auth.js";
 const auth = getAuth();
